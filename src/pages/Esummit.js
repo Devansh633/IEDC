e4bdc31a355ef86da11e7fe88c97a758cf7b647d
Devansh Pehlajani <devanshrockz11@gmail.com>
2023-12-03 23:22:46 +0530

    done

--- src/pages/Esummit.js
+++ src/pages/Esummit.js
@@ -2,17 +2,21 @@
 import React, { useState, useEffect } from 'react';
 import { Footer, Navbar, Hero } from '../components';
 import StickyComponents from '../components/StickyComponents';
+import Loader from '../assets/data/Animation - 1701624849592.json';
+import Lottie from 'lottie-react';
 
 const Esummit = () => {
   const [imageSource, setImageSource] = useState('images/fffbg3.webp'); // Initial image source
   const [imageSource1, setImageSource1] = useState('images/esum1.webp');
+  const [showLoader, setShowLoader] = useState(true);
+
   useEffect(() => {
     const handleResize = () => {
       const screenWidth = window.innerWidth;
       setImageSource(screenWidth > 768 ? 'images/fffbg3.webp' : 'images/fffbg4.webp');
       setImageSource1(screenWidth > 768 ? 'images/esum1.webp' : 'images/fffbg5.webp');
     };
-// 
+
     handleResize();
 
     // Add resize event listener
@@ -24,23 +28,44 @@ const Esummit = () => {
     };
   }, []);
 
+  useEffect(() => {
+    // Show loader for 3 seconds
+    const loaderTimeout = setTimeout(() => {
+      setShowLoader(false);
+    }, 5000);
+
+    // Clear timeout on component unmount or when loader is hidden
+    return () => {
+      clearTimeout(loaderTimeout);
+    };
+  }, []);
+
   return (
     <div className="relative">
-      <Navbar />
-      <div className='flex flex-col items-center justify-center'>
-      <img
-          src={imageSource1}
-          alt='Zoomed Out Image'
-          className="object-cover object-center w-screen  h-auto md:scale-90 md:mt-0 mt-[4rem] mb-[2rem] "
-        />
-        <img
-          src={imageSource}
-          alt='Zoomed Out Image'
-          className="object-cover object-center max-w-full h-full transform scale-[100%]"
-        />
-        <StickyComponents />
-      </div>
-      <Footer />
+      {showLoader && (
+        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
+          <Lottie animationData={Loader} className="w-[15rem]" />
+        </div>
+      )}
+      {!showLoader && (
+        <>
+          <Navbar />
+          <div className="flex flex-col items-center justify-center">
+            <img
+              src={imageSource1}
+              alt="Zoomed Out Image"
+              className="object-cover object-center w-screen h-auto md:scale-90 md:mt-0 mt-[4rem] mb-[2rem]"
+            />
+            <img
+              src={imageSource}
+              alt="Zoomed Out Image"
+              className="object-cover object-center max-w-full h-full transform scale-[100%]"
+            />
+            <StickyComponents />
+          </div>
+          <Footer />
+        </>
+      )}
     </div>
   );
 };
